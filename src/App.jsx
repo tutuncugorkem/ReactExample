6a05d1a17da6ec8689062387bfb277ad04ae7e5a
adm_gorkem.tutuncu <adm_gorkem.tutuncu@monsternotebook.com>
2025-12-05 16:06:24 +0300

    be+fe connection problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
-import "./App.jsx";
-
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [message, setMessage] = useState("");
   const [count, setCount] = useState(0);
+
+  // ⬇⬇⬇ Backend'e istek atan FETCH burada ⬇⬇⬇
+  useEffect(() => {
+    fetch("http://localhost:3001/api/message")
+      .then((res) => res.json())
+      .then((data) => {
+        setMessage(data.message);
+      })
+      .catch((err) => {
+        console.error("Backend bağlantı hatası:", err);
+      });
+  }, []);
 
   return (
     <div style={{ padding: 20 }}>
       <h1>Mini React Uygulaması 🚀</h1>
+
+      <p><strong>Backend Mesajı:</strong> {message}</p>
+
       <p>Butona {count} kez tıkladın.</p>
-      <button onClick={() => setCount(count + 1)}>
+
+      <button className="pulse-button" onClick={() => setCount(count + 1)}>
         Tıkla!
       </button>
     </div>
